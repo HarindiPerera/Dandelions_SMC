4de3fd4279790125b71dd7fa8e4b4059eabcbb9f
Jake Sheath <z5109829@ad.unsw.edu.au>
2022-12-06 11:45:28 +1100

    Playing around with interrupts

--- sample_project/main/main.c
+++ sample_project/main/main.c
@@ -16,23 +16,87 @@
 #include "driver/i2c.h"             // for the ADC 
 #include "unity_test_runner.h"      // added for i2c functionality
 #include "sdkconfig.h"              // added for i2c functionality
-
+#include "esp_intr_alloc.h"         // allows for interupts allocation
 #include "Dand_SMC.h"               // Definitions of all the defined constants and basic motor functionality functions
 #include "ADC.h"                    // ADC functions
-
+#include "soc/soc.h"                // This defines the external interrupt sources. as ETS_<>_INTR_SOURCE
 int error = 0;                      // Decaire error variable
 int pulses =500;                    // change this based on the number of pulses in either direction we want on TVAC day
 
+
+/* Non internal interrupts slots in both CPU cors are wired to an interrupts multiplexer which can be use to route any external interrupts source to any of these intrrupts slots
+Allocating an external intrrupts will alwasy allocate it on the core that does the allocation
+Freeing is also bound to the core. 
+Disableing and enableing external interupts can be done from either core. 
+Multiple external interrupts sources can share an interrupts slot by passing ESP_INTR_FLAG_SHARED as a flag to esp_inter_alloc()
+
+Definately think i am going to steal from the interrupt example program. 
+
+so lets have lunch and then do that. 
+i am freaking starving. 
+
+
+*/ 
+
+
+
+
+// attempts at interupts 
+
+void Boi(){
+    printf("You have interupted me you absolute ding dong\n");
+
+}
+
+intr_handler_t InterruptBoi = Boi; // intr
+
+
+
+
 //___________________________________________APP__MAIN_______________________________________________
 
+
+
 void app_main(void){
-// SETUP 
+
+esp_intr_alloc(ETS_GPIO_INTR_SOURCE, 0, InterruptBoi ,NULL, NULL);
+/*The interupt will always be allocatted on the core that runs this function
+this finds an interrupts that matches the restrictions given in the flags parameters, maps the given interrupts source to it and hoks up the given handler
+(with optional arguments), it can also return a handle for the interrupts as well. 
+if ESP_INTR_FLAG_IRAM flag is use and the andler address is not in IRAM or RTC_FAST_MEM then ESP_ERROR_INVALID_ARG is returned. 
+
+source - The interupts source-> needs to be one of the ETS_<>_INTR_SOURCE sources defined in the header. 
+flags - flags - and ORread mask of the ESP_INTR_FLAG_<> defines. Thes restrict the choice of interrupts that this routine can choose from. 
+    if the value is 0 it will default to allocating non-shared intrrupts level 1, 2 or 3. 
+handler- Must be NULL when an intrrupts of level >3 is requested because these types of intrrupts arnt't C -callable. 
+*/
+
 char ch ='0';
 setupGpios();                   // Setup function for all the GPIOS in the SMC_D2 configuration
 i2c_master_cmd_set();           // Setup the i2c bus 
 printf("Dandelions SMCD2: Ready in idle state.\n");               // User instructions
 printf("IDLE: Press 'r' to run experiment once and 'q' to quit/restart.\n");  // User instructions
 gpio_set_level(MVEN, 1);        // Enable Motor voltage from E-Fuse
+
+/*
+temp_sensor_config_t temp_sens = {
+    range_min =0.0;
+    range_min =60.0;
+};
+temp_sensor_set_config(temp_sens);
+float tsens =0;              // temprture value 
+*/
+// Test code for an internal temp sensor. 
+/*
+temperature_* temp_handle = NULL; 
+temperature_sensor_config_t temp_sensor = {
+    .range_min = 0.0; 
+    .range_max = 80.0;
+};
+ESP_ERROR_CHECK(temperature_sensor_install(&temp_sensor, &temp_handle));
+float tsen_out; 
+temperature_sensor_install();
+*/
 
 
 // INFINITE LOOP // 
@@ -102,7 +166,17 @@ gpio_set_level(MVEN, 1);        // Enable Motor voltage from E-Fuse
         }else {
             vTaskDelay(100/ portTICK_PERIOD_MS);    // wait 0.1 second if in idle state
             pingWatchdog();                             // ping the watchdog if in idle state
+
+           // temp_sensor_read_celsius(&tsens);
+           // printf("IDLE: Current core temp = %f\n",tsens);
+
+            /*       Test code for checking tempreture stuff 
+            ESP_ERROR_CHEKC(temperature_sensor_enable(temp_handle));    // Enable the temperaure sensor
+            ESP_ERROR_CHECK(temperature_sensor_get_celsius(temp_handle,&tsens_out));    // get tempreture data
+            ESP_ERROR_CHECK(temperature_sensor_disable(temp_handle));   // disable temp sensor to save power
+            */
         }
+
     }
 
     /*CLEANUP*/
